228fa48ce5bd6ccc7e0a1a1f69762d469439862f
ramsharan2003 <168457153+ramsharan2003@users.noreply.github.com>
2024-08-28 19:09:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('Thisissecret') 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('psql "postgres://default:x1SfVbRac7vX@ep-solitary-dust-a1cazav3.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgres://default:************@ep-solitary-dust-a1cazav3.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('thisisjwt') 
 
